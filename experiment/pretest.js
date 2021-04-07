73ee4cf74dfb1bf6fa495d802c67e4f6933c5dcd
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-04-07 14:32:50 +0530

    Added pretest and post-test sections.

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -88,26 +88,24 @@
      /////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-     const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
-            answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
-            },
-            correctAnswer: "a" ///// Write the correct option inside double quotes
-        },
+     const myQuestions = [
+	     {
+		     question: "1.Time taken for one complete oscillation is called Time Period of the oscillator.", ///// Write the question inside double quotes
+		     answers: {
+			     a: "True", ///// Write the option 1 inside double quotes
+			     b: "False", ///// Write the option 2 inside double quotes
+		     },
+		     correctAnswer: "a" ///// Write the correct option inside double quotes
+	     },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
-
-
-
+	     {
+		     question: "2.Time period of the oscillator is constant for given values of mass, length and initial conditions.", ///// Write the question inside double quotes
+		     answers: {
+			     a: "True", ///// Write the option 1 inside double quotes
+			     b: "False", ///// Write the option 2 inside double quotes
+		     },
+		     correctAnswer: "a" ///// Write the correct option inside double quotes
+	     },
      ];
 
 
